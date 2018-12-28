8054695feb2eee0921c318073181137fbcd5cc0c
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2018-12-27 21:18:43 -0500

    Make 404 nicer

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -8,13 +8,12 @@ class NotFoundRoute extends React.Component {
   render() {
     const { i18n: { lang } } = this.props
     const title = {
-      en: "Uh oh, there's nothing there!",
-      fr: 'Oh oh, il n’y a rien ici !',
+      en: "Hmmm, there's nothing there…",
+      fr: 'Hum, il n’y a rien ici…',
     }[lang]
     const content = {
-      en: "You just hit a page that doesn't exist... the sadness.",
-      fr:
-        'Vous venez d’arriver sur une page qui n’existe pas… quelle tristesse.',
+      en: "You just hit a page that doesn't exist.",
+      fr: 'Vous venez d’arriver sur une page qui n’existe pas.',
     }[lang]
 
     return (
@@ -25,6 +24,14 @@ class NotFoundRoute extends React.Component {
             <div className="content__inner">
               <div className="page">
                 <h1 className="page__title">{title}</h1>
+                <iframe
+                  width="560"
+                  height="315"
+                  src="https://www.youtube.com/embed/CDa-0adnlmk?controls=0"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                />
                 <div className="page__body">
                   <p>{content}</p>
                 </div>
