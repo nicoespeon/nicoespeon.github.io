bedd5d27f8304dd94496758bcc48163428499a1f
Nicolas Carlo <nicolas.carlo@busbud.com>
2019-11-25 18:01:35 -0500

    Updates

--- sw.js
+++ sw.js
@@ -26,21 +26,21 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-976c85949a3989597a8c.js"
+    "url": "webpack-runtime-705f4eadbdedd653e1f3.js"
   },
   {
-    "url": "app-4b69be65322cef6f2922.js"
+    "url": "app-a28781cee54de6f2bd9c.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-5f2b74aaa9780afd7bf0.js"
   },
   {
     "url": "index.html",
-    "revision": "f21396c2b50df0ad6771c4b57f742248"
+    "revision": "a5125b2ab6921ea2f96a2d7c025dde9e"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "fb4ab74a1c14002569dfe6b04dc1c8be"
+    "revision": "597eea2741ffdfc087280d90d5359cd5"
   },
   {
     "url": "1.06404778f6bbd2eb332c.css"
@@ -52,24 +52,24 @@ self.__precacheManifest = [
     "url": "component---src-pages-index-jsx-f009cd138e1df3aeee40.js"
   },
   {
-    "url": "0-a7e92077253379ca9505.js"
+    "url": "0-1fc3bea78341134a4e47.js"
   },
   {
-    "url": "1-11aa4c76b5a3f5afa7db.js"
+    "url": "1-47e797bfb0f5cb3dc723.js"
   },
   {
     "url": "2-9d62d26dd2cdaa76a942.js"
   },
   {
-    "url": "static/d/342/path---index-6a9-g83BFI2MoJWT4FjCFz7S4oJAOE.json",
-    "revision": "62b49139c88b6c39a9ad885182c45096"
+    "url": "static/d/266/path---index-6a9-vVxD2qtGkyUyWxTz0iaBZJGM8.json",
+    "revision": "9df92ed906c999f28f6da66c772b4c35"
   },
   {
     "url": "component---src-pages-404-jsx-7856d8e49ab062d7bb70.js"
   },
   {
-    "url": "static/d/465/path---404-html-516-62a-vKf8dUbNQtlVOTOd8BItuslxs.json",
-    "revision": "db48381469fe455c7e2165ae0ddd77e3"
+    "url": "static/d/248/path---404-html-516-62a-17zMIRn17FUPXyHQEWJ5UyWuY.json",
+    "revision": "bce0b2ad48db90a60bb0d210b284d6a4"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
